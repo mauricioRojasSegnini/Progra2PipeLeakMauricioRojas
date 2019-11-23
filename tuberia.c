831ba61332b8c8b7afb6939137df7c5c1d510da1
mauricioRojasSegnini <56244465+mauricioRojasSegnini@users.noreply.github.com>
2019-11-23 10:16:29 -0600

    Update tuberia.c

--- tuberia.c
+++ tuberia.c
@@ -1,4 +1,3 @@
-
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
@@ -14,12 +13,18 @@
  * v.1
 */
 
+/*
+ *Esta es la estructura para las fuentes de agua y los drenajes 
+ * contiene la fila y la columna y la direccion en la que se encuentra 
+ */
 typedef struct waterTapsOrDrains{
-	int x;
-	int y;
+	int xRow;
+	int yColumn;
 	char dir;
 }TapsOrDrains;
-
+/*
+ * esta es la estructura para los requerimientos que solicita el usuario para ejecutar el programa
+ */
 typedef struct Requirements{
     //create variable for the action required 
     char action[40];
@@ -31,6 +36,7 @@ typedef struct Requirements{
     char folderName[40];
 }Requirements;
 
+//subrutinas del programa
 void goTo(int **matriz,int row, int column, char dir,TapsOrDrains *tapsOrDrainsArray, int taps, int drains, int matrizRows, int matrizColumns,int **copyMatriz,Requirements game);
 void actionsMenu( int** matriz,char action[], TapsOrDrains *tapsOrDrainsArray, int taps, int drains,int matrizRows,int matrizColumns, Requirements game);
 void destruirMatriz(int **matriz, const int matrizRows);
@@ -43,15 +49,19 @@ void printLeak(int x, int y, char dir,Requirements game);
 int** copy(int** matriz, int matrizRows, int matrizColumns);
 /*
  * this is the main program where it contains all the subroutines required to solve, convert or validate the level
- * and the subrputine to print in the format that the user wants  
+ * and the subroutine to print in the format that the user wants  
  * Efe:
  * Mod:
  * Req: it needs the arguments of the action, the print format, the read format and the file name 
  */
 int main(int args_count, char *args[]){
+	//crear un array para las fuentes de agua y los drenajes
 	TapsOrDrains* tapsOrDrainsArray;
+	// crear la estructura del juego
     Requirements game;
+    //crear la matriz para la tuberia
     int** matriz;
+    //crear las variables 
     int waterTaps, drains, matrizRows, matrizColumns;
     int error=0;
     int rowWaterTap, columnWaterTap;
@@ -60,21 +70,26 @@ int main(int args_count, char *args[]){
     
     //indetify what arguments are giving 
     strcpy(game.printFormat,"-ot" ); //default format
-    strcpy(game.folderName,""); //NULL
+    strcpy(game.folderName,""); //default format
+    //repetir hasta el fin de los argumentos
     for(int indice=1; indice< args_count; indice++){
+		// si el usuario ingreso alguno de las acciones del programa
         if( ((strcmp(args[indice],"validate" ) == 0) ) || ( (strcmp(args[indice],"solve" ) == 0)) || ((strcmp(args[indice],"convert" ) == 0) ) ){
-            //action
+            //asignarle el argumento a la variable de acciones
             strcpy(game.action,args[indice]);
         }else{
+			// si el usuario ingreso alguno de los fomatos de lectura del programa
 			if( ((strcmp(args[indice],"-it" ) == 0)) || ((strcmp(args[indice],"-ib" ) == 0)) ){
-				//formato de lectura
+				//asignarle el argumento ingresado a l a variable de lectura
 				strcpy(game.readingFormat,args[indice]);
 			}else{
+				// si el usuario ingreso alguno de los fomatos de salida del programa
 				if( (strcmp(args[indice],"-ot" ) == 0) || (strcmp(args[indice],"-ob" )== 0)|| (strcmp(args[indice],"-o" )== 0)) {
-					//formato de salida
+					//asignarle el argumento ingresado a la variable de salida
 					strcpy(game.printFormat,args[indice]);
 				}else{
-					//nombre del archivo 
+					//si ingreso un nombre de archivo 
+					//asignarle a la variable del nombre de archivo
 					strcpy(game.folderName,args[indice]);
 				}
 			}
@@ -82,29 +97,33 @@ int main(int args_count, char *args[]){
     }
     //if the arguments is -it it reads from the standar input
     if(strcmp(game.readingFormat,"-it" ) == 0){
-		
+		//si no ingreso un archivo de lectura entonces
 		if(strcmp(game.folderName,"" ) == 0){
-			
+			//leer desde la entrada estandar
 			scanf("%d%d%d%d", &matrizRows, &matrizColumns, &waterTaps, &drains);
 			tapsOrDrainsArray = malloc((waterTaps+drains) * sizeof *tapsOrDrainsArray);
+			//repetir hasta la cantidad de fuentes de agua
 			for(int numOfWTaps=0;numOfWTaps<waterTaps;numOfWTaps++){
 				scanf("%d %d %c",&rowWaterTap,&columnWaterTap,&dir);
-				tapsOrDrainsArray[numOfWTaps].x=rowWaterTap;
-				tapsOrDrainsArray[numOfWTaps].y=columnWaterTap;
+				tapsOrDrainsArray[numOfWTaps].xRow=rowWaterTap;
+				tapsOrDrainsArray[numOfWTaps].yColumn=columnWaterTap;
 				tapsOrDrainsArray[numOfWTaps].dir =dir;
 			}
+			//repetir hasta la cantidad de drenajes
 			for(int numOfDrains=waterTaps;numOfDrains<(waterTaps+drains);numOfDrains++){
 				scanf("%d %d %c",&rowDrain,&columnDrain,&dir);
-				tapsOrDrainsArray[numOfDrains].x=rowDrain;
-				tapsOrDrainsArray[numOfDrains].y=columnDrain;
+				tapsOrDrainsArray[numOfDrains].xRow=rowDrain;
+				tapsOrDrainsArray[numOfDrains].yColumn=columnDrain;
 				tapsOrDrainsArray[numOfDrains].dir =dir;
 			}
 			matriz= crearMatriz(matrizRows, matrizColumns);
 			error=cargarMatriz(matriz, matrizRows, matrizColumns);
 		}else{
+			//si ingreso un archivo de lectura
 			FILE *fp;
 			char blanco;
 			fp = fopen(game.folderName, "r");
+			//leer los datos de entrada
 			matrizRows=fgetc(fp)-48;
 			blanco=fgetc(fp);
 			matrizColumns=fgetc(fp)-48;
@@ -115,7 +134,7 @@ int main(int args_count, char *args[]){
 			blanco=fgetc(fp);
 			blanco=fgetc(fp);
 			tapsOrDrainsArray = malloc((waterTaps+drains) * sizeof *tapsOrDrainsArray);
-			
+			//leer las fuentes de agua
 			for(int numOfWTaps=0;numOfWTaps<waterTaps;numOfWTaps++){
 				rowWaterTap=fgetc(fp)-48;
 				blanco=fgetc(fp);
@@ -123,11 +142,11 @@ int main(int args_count, char *args[]){
 				blanco=fgetc(fp);
 				dir=fgetc(fp);
 				blanco=fgetc(fp);
-				tapsOrDrainsArray[numOfWTaps].x=rowWaterTap;
-				tapsOrDrainsArray[numOfWTaps].y=columnWaterTap;
+				tapsOrDrainsArray[numOfWTaps].xRow=rowWaterTap;
+				tapsOrDrainsArray[numOfWTaps].yColumn=columnWaterTap;
 				tapsOrDrainsArray[numOfWTaps].dir =dir;
 			}
-			
+			//leer los drenajes
 			for(int numOfDrains=waterTaps;numOfDrains<(waterTaps+drains);numOfDrains++){
 				rowDrain=fgetc(fp)-48;
 				blanco=fgetc(fp);
@@ -135,11 +154,13 @@ int main(int args_count, char *args[]){
 				blanco=fgetc(fp);
 				dir=fgetc(fp);
 				blanco=fgetc(fp);
-				tapsOrDrainsArray[numOfDrains].x=rowDrain;
-				tapsOrDrainsArray[numOfDrains].y=columnDrain;
+				tapsOrDrainsArray[numOfDrains].xRow=rowDrain;
+				tapsOrDrainsArray[numOfDrains].yColumn=columnDrain;
 				tapsOrDrainsArray[numOfDrains].dir =dir;
 			}
+			//crear la matriz de tuberia 
 			matriz= crearMatriz(matrizRows, matrizColumns);
+			//leer los valores de las tuberias
 			error=cargarMatrizDeFile(matriz, matrizRows, matrizColumns,fp);
 			fclose(fp);
 			
@@ -161,14 +182,17 @@ int main(int args_count, char *args[]){
     return 0;
 }
 /*
- * Efe:
- * Mod:
- * Req:
+ * Efe: Esta es la subrutina para ejecutar las accion que el usuario ingreso en los argumentos 
+ * Mod: no modifica solo ejecuta subrutinas
+ * Req: requiere de la matriz de tuberias, la accion requerida, las fuentes de agua y los drenajes, y los parametros de la matriz
  */
 void actionsMenu(int** matriz, char action[], TapsOrDrains *tapsOrDrainsArray,int taps, int drains,int matrizRows,int matrizColumns,Requirements game){
+	//si el usuario ingreso la accion de validar
 	 if(strcmp( action,"validate" ) == 0){
-		 int **copyMatriz=copy(matriz, matrizRows, matrizColumns);
-		validateLevel( matriz, tapsOrDrainsArray[0].x-1,tapsOrDrainsArray[0].y-1, tapsOrDrainsArray[0].dir,tapsOrDrainsArray,taps,drains, matrizRows,matrizColumns,copyMatriz, game);
+	//crear una copia de la matriz para ir marcando las celdas visitadas 
+	int **copyMatriz=copy(matriz, matrizRows, matrizColumns);
+	//ejecutar subrutina para validar la matriz de tuberia
+	validateLevel( matriz, tapsOrDrainsArray[0].xRow-1,tapsOrDrainsArray[0].yColumn-1, tapsOrDrainsArray[0].dir,tapsOrDrainsArray,taps,drains, matrizRows,matrizColumns,copyMatriz, game);
 	}else{
 		if(strcmp(action,"solve" ) == 0){
 			printf("subrutina para resolver\n");
@@ -183,9 +207,9 @@ void actionsMenu(int** matriz, char action[], TapsOrDrains *tapsOrDrainsArray,in
   
 }
 /*
- * Efe:
- * Mod:
- * Req:
+ * Efe: Esta es la subrutina para crear la matriz de tuberias con malloc
+ * Mod: crea la matriz de tuberias
+ * Req: requiere de los parametros de tamano de la matriz
  */
 int** crearMatriz(const int matrizRows, const int matrizColumns){
     int **matriz= malloc(matrizRows*sizeof(int *));
@@ -195,9 +219,9 @@ int** crearMatriz(const int matrizRows, const int matrizColumns){
     return matriz;
 }
 /*
- * Efe:
- * Mod:
- * Req:
+ * Efe: Esta es la subrutina para eliminar la memoria de la matriz creada
+ * Mod: elimina la matriz de tuberia 
+ * Req: requiere de la matriz
  */
 void destruirMatriz(int **matriz, const int matrizRows){
 	assert(matriz);
@@ -207,20 +231,23 @@ void destruirMatriz(int **matriz, const int matrizRows){
 	free(matriz);
 }
 /*
- * Efe:
- * Mod:
- * Req:
+ * Efe: Esta es la subrutina para cargar la matriz
+ * Mod: modifica los valores de las celdas de la matriz por los valores de la entrada
+ * Req: requiere de la matriiz y de los parametros de tamano
  */
 int cargarMatriz(int ** matriz, const int matrizRows, const int matrizColumns){
     char letra;
     int num;
+    //repetir por filas y columnas
     for(int filas_cont=0;filas_cont<matrizRows; filas_cont++){
         for(int columna_cont=0;columna_cont<matrizColumns; columna_cont++){
+			//si es un numero entre 1 y 9
 			if((scanf("%d",&num)==1)&&(num<=9)){
+				//leer valor y asignarlo
 				matriz[filas_cont][columna_cont]=num;
 			}else{
+				//si es una letra cambiarlo por el valor decimal 
 				scanf("%c",&letra);
-				
 				if(letra=='A'){
 					num=10;
 				}else if(letra=='B'){
@@ -244,6 +271,11 @@ int cargarMatriz(int ** matriz, const int matrizRows, const int matrizColumns){
 	return 0;
 
 }
+/*
+ * Efe: esta es la subrutina para cargar la matriz si el usuario ingreso un archivo de lectura
+ * Mod: carga la matriz con los valores de entrada 
+ * Req: de la matriz de tuberia, los parametros de tamano y del archivo de lectura
+ */
 int cargarMatrizDeFile(int ** matriz, const int matrizRows, const int matrizColumns,FILE *fp){
 	int num;
 	char blanco;
@@ -264,9 +296,9 @@ int cargarMatrizDeFile(int ** matriz, const int matrizRows, const int matrizColu
     return 0;
 }
 /*
- * Efe:
+ * Efe: imprime la matriz de tuberia
  * Mod:
- * Req:
+ * Req: de la matriz y de los parametros de tamano, el formato de salida ingresado por el usuario y el folder de salida
  */
 void printGame(int **matriz, const int matrizRows, const int matrizColumns, char printFormat[], char folder[]){
 	// if the print format is -ot or its the default format, print in the standard output
@@ -283,7 +315,6 @@ void printGame(int **matriz, const int matrizRows, const int matrizColumns, char
 					sprintf(celda,"%d",matriz[filas_cont][columna_cont]);
 					fputs(celda,fichero);
 					fputs(" ",fichero);
-					printf("%s ",celda);
 					
 				}else{
 					//imprimir en formato nomal
@@ -296,9 +327,12 @@ void printGame(int **matriz, const int matrizRows, const int matrizColumns, char
 		}
 	}
 }
+/*
+ * Efe: Esta es la subrutina para validar la matriz de tuberia, si hay alguna fuga el programa lo va a detectar e indicar donde es la fuga 
+ * Mod:
+ * Req: de la matriz, parametros de tamanos de matriz, las funetes y drenajes, la matriz para las celdas visitadas y los requerimientos del juego
+ */
 void validateLevel(int** matriz, int row, int column, char dir,TapsOrDrains *tapsOrDrainsArray,int taps, int drains, int matrizRows, int matrizColumns,int **copyMatriz,Requirements game){
-	//bool leak=false;
-	//printf("inicialice el boola false \n");
 	int rowCell=row;
 	int columnCell=column;
 	//Subrutina para evaluar la celda del nivel
@@ -421,8 +455,12 @@ void validateLevel(int** matriz, int row, int column, char dir,TapsOrDrains *tap
 //Fin de la subrutina 
 }
 
-//Subrutina para ir a la direccion requerida 
-//Se ocupa por parámetros la fila y la columna y el punto cardinal 
+/*
+ * Efe: Subrutina para ir a la direccion requerida y condiciona las celdas si hay alguna fuga la reporta
+ * Mod:
+ * Req: de la matriz, parametros de tamanos de matriz, las funetes y drenajes, la matriz para las celdas visitadas y los requerimientos del juego
+ * Se ocupa por parámetros la fila y la columna y el punto cardinal 
+ */
 void goTo(int **matriz,int row, int column, char dir,TapsOrDrains *tapsOrDrainsArray, int taps, int drains, int matrizRows, int matrizColumns,int **copyMatriz,Requirements game){
 	//Condicionar si la celda se salió del rango
 	if((row>=0 && column>=0) &&(row<matrizRows&&column<matrizColumns)){
@@ -468,13 +506,10 @@ void goTo(int **matriz,int row, int column, char dir,TapsOrDrains *tapsOrDrainsA
 				}
 			}	
 		}
-			//imprimir fuga la fila y la columna y la etiqueta
-			//retornar
 		//Condicionar si la celda tiene otro numero
 		if(matriz[row][column]>=0 && matriz[row][column]<=15){
 			//Ejecutar subrutina para evaluar la siguiente celda
 			validateLevel(matriz, row, column, dir, tapsOrDrainsArray, taps,  drains,  matrizRows,  matrizColumns,copyMatriz, game);
-			//return true;
 		} 
 	}else{
 		int rowC=row+1;
@@ -493,7 +528,7 @@ void goTo(int **matriz,int row, int column, char dir,TapsOrDrains *tapsOrDrainsA
 		}
 		//Condicionar si la celda es la fuente o el desague
 		for(int waterTapOrDrains=0; waterTapOrDrains<taps+drains; waterTapOrDrains++){
-			if( (tapsOrDrainsArray[waterTapOrDrains].dir==dir)&&(tapsOrDrainsArray[waterTapOrDrains].x==rowC)&&(tapsOrDrainsArray[waterTapOrDrains].y==colC) ){
+			if( (tapsOrDrainsArray[waterTapOrDrains].dir==dir)&&(tapsOrDrainsArray[waterTapOrDrains].xRow==rowC)&&(tapsOrDrainsArray[waterTapOrDrains].yColumn==colC) ){
 				return ;
 			}
 		}
@@ -502,6 +537,11 @@ void goTo(int **matriz,int row, int column, char dir,TapsOrDrains *tapsOrDrainsA
 	}
 //Fin de la subrutina 
 }
+/*
+ * Efe: imprime la fuga de tuberia
+ * Mod:
+ * Req: de la posicion de la fuga y la direccion
+ */
 void printLeak(int x, int y, char dir,Requirements game){
 		FILE * fichero;
 		fichero = fopen( game.folderName ,"a");
@@ -567,6 +607,11 @@ void printLeak(int x, int y, char dir,Requirements game){
 		}
 		
 }
+/*
+ * Efe: copia la matriz de tuberia
+ * Mod:
+ * Req: de la matriz y de los parametros de tamano
+ */
 int** copy(int** matriz, int matrizRows, int matrizColumns){
 	int **copy= malloc(matrizRows*sizeof(int *));
     for(int i=0; i<matrizRows; i++){
@@ -579,6 +624,5 @@ int** copy(int** matriz, int matrizRows, int matrizColumns){
 	}
 	return copy;
 }
-
 
 
